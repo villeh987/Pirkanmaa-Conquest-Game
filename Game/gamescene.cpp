c964edf7e2db182dc0b0deeb5295e864e6203eaa
Kasper <jarvin46@tuni.fi>
2019-11-19 22:42:27 +0200

    Added first version of water class and deleted some methods from gamescene

--- Game/gamescene.cpp
+++ Game/gamescene.cpp
@@ -9,70 +9,29 @@ namespace Game {
 
 GameScene::GameScene(QWidget* parent, int width, int height, int scale):
     QGraphicsScene(parent),
-    m_mapBoundRect(nullptr)
-    //m_width(15),
-    //m_height(15),
-    //m_scale(75)
+    _mapBoundRect(nullptr)
+
 {
-    setSize(width, height);
-    setScale(scale);
+    scene_width = width;
+    scene_height = height;
+    scene_scale = scale;
+
+    QRect game_rect = QRect(-1, -1, (scene_width * scene_scale)+2, (scene_height * scene_scale)+2);
+
+    addRect(game_rect, QPen(Qt::magenta, 10));
+    setSceneRect(game_rect);
+    _mapBoundRect = itemAt(game_rect.topLeft(), QTransform());
+
 }
 
-void GameScene::setSize(int width, int height)
+
+void GameScene::drawMapItem(std::shared_ptr<Course::GameObject> obj)
 {
-    if ( width >= SCENE_WIDTH_LIMITS.first && width <= SCENE_WIDTH_LIMITS.second )
-    {
-        m_width = width;
-    }
-    if ( height >= SCENE_HEIGHT_LIMITS.first && height <= SCENE_HEIGHT_LIMITS.second )
-    {
-        m_height = height;
-    }
-    resize();
-}
-
-void GameScene::setScale(int scale)
-{
-    if ( scale >= SCENE_SCALE_LIMITS.first && scale <= SCENE_SCALE_LIMITS.second )
-    {
-        m_scale = scale;
-    }
-    resize();
-}
-
-void GameScene::resize()
-{
-    if ( m_mapBoundRect != nullptr ){
-        QGraphicsScene::removeItem(m_mapBoundRect);
-    }
-
-    // päätetään että tiedetään mapin koko aina, eli ei lasketa sitä turhaan.
-    QRect rect = QRect(m_width-11, m_height-11, (m_width * m_scale)+2, (m_height * m_scale)+2);
-
-    addRect(rect, QPen(Qt::black));
-    setSceneRect(rect);
-    m_mapBoundRect = itemAt(rect.topLeft(), QTransform());
-    // Draw on the bottom of all items
-    m_mapBoundRect->setZValue(-1);
-}
-
-int GameScene::getScale() const
-{
-    return m_scale;
-}
-
-std::pair<int, int> GameScene::getSize() const
-{
-    return {m_width, m_height};
-}
-
-void GameScene::drawItem(std::shared_ptr<Course::GameObject> obj)
-{
-    Game::MapItem* nItem = new Game::MapItem(obj, m_scale);
+    Game::MapItem* nItem = new Game::MapItem(obj, scene_scale);
     addItem(nItem);
 }
 
-void GameScene::removeItem(std::shared_ptr<Course::GameObject> obj)
+void GameScene::removeMapItem(std::shared_ptr<Course::GameObject> obj)
 {
     QList<QGraphicsItem*> items_list = items(obj->getCoordinate().asQpoint());
     if ( items_list.size() == 1 ){
@@ -88,7 +47,7 @@ void GameScene::removeItem(std::shared_ptr<Course::GameObject> obj)
 
 }
 
-void GameScene::updateItem(std::shared_ptr<Course::GameObject> obj)
+void GameScene::updateMapItem(std::shared_ptr<Course::GameObject> obj)
 {
     QList<QGraphicsItem*> items_list = items();
     if ( items_list.size() == 1 ){
@@ -112,7 +71,7 @@ bool GameScene::event(QEvent *event)
 
         if ( sceneRect().contains(mouse_event->scenePos())){
 
-            QPointF point = mouse_event->scenePos() / m_scale;
+            QPointF point = mouse_event->scenePos() / scene_scale;
 
             point.rx() = floor(point.rx());
             point.ry() = floor(point.ry());
@@ -121,7 +80,7 @@ bool GameScene::event(QEvent *event)
 
 
 
-            QGraphicsItem* pressed = itemAt(point * m_scale, QTransform());
+            QGraphicsItem* pressed = itemAt(point * scene_scale, QTransform());
 
             //QRectF current = static_cast<Course::SimpleMapItem*>(pressed)
              //       ->boundingRect();
@@ -133,7 +92,7 @@ bool GameScene::event(QEvent *event)
             //qDebug() << "comp_x:" << qreal(current_compare.x()) << "comp_y:" << qreal(current_compare.y()) << "comp_width:" << qreal(current_compare.width()) << "comp_height:" << qreal(current_compare.height());
 
 
-            if ( pressed == m_mapBoundRect ){
+            if ( pressed == _mapBoundRect ){
                 qDebug() << "Click on map area.";
 
             } else if (pressed == highlight_) {
