ea090a5015f45a97dbc5b42e05c9de9cca81ff02
Vilhelmi Lehtonen <vilhelmi.lehtonen@tuni.fi>
2019-11-30 15:07:19 +0200

    Added documentation for Game_workers

--- Game/Game_workers/miner.hh
+++ Game/Game_workers/miner.hh
@@ -6,9 +6,33 @@
 
 namespace Game {
 
+
+/**
+ * @brief The Miner class represents a miner worker in the game.
+ *
+ * Miner efficiency may e found in constants.hh!
+ * Miner is extremely useful on Rock type tiles.
+ *
+ */
 class Miner : public Course::WorkerBase
 {
 public:
+
+    /**
+     * @brief Disabled parameterless constructor.
+     */
+    Miner() = delete;
+
+    /**
+     * @brief Constructor for the class.
+     *
+     * @param eventhandler points to the GameEventHandler.
+     * @param objectmanager points to the ObjectManager.
+     * @param owner points to the owning player.
+     * @param tilespaces (Optional) How many tilespaces does the building take.
+     * @param cost (Optional) How many resources does it cost to assign worker.
+     * @param efficiency (Optional) How good is the worker at his job.
+     */
     Miner(const std::shared_ptr<Course::iGameEventHandler> &eventhandler,
           const std::shared_ptr<Course::iObjectManager> &objectmanager,
           const std::shared_ptr<Course::PlayerBase> &owner,
@@ -16,8 +40,22 @@ public:
           const Course::ResourceMap &cost = GameResourceMaps::MINER_RECRUITMENT_COST,
           const Course::ResourceMapDouble &efficiency = GameResourceMaps::MINER_EFFICIENCY);
 
+
+    /**
+     * @brief Default destructor.
+     */
+    virtual ~Miner() = default;
+
+    /**
+     * @brief getType Returns a string describing objects type.
+     * @return std::string that represents Object's type.
+     * @post Exception guarantee: No-throw
+     */
     virtual std::string getType() const;
 
+    /**
+     * @brief Performs the Worker's default action.
+     */
     virtual void doSpecialAction() override;
 };
 
--- Game/Game_workers/teekkari.hh
+++ Game/Game_workers/teekkari.hh
@@ -6,10 +6,35 @@
 
 namespace Game {
 
-
+/**
+ * @brief The Teekkari class represents a Teekkari worker in the game.
+ *
+ * Teekkari efficiency may e found in constants.hh!
+ * Teekkari is the most powerful worker in the Game, as it can destoy opponent
+ * Player's buildings.
+ * Teekkari can also have a Teekkarifight against opponents Teekkari.
+ * Loser of the Teekkarifight loses his Teekkari - and also his honor.
+ *
+ */
 class Teekkari : public Course::WorkerBase
 {
 public:
+
+    /**
+     * @brief Disabled parameterless constructor.
+     */
+    Teekkari() = delete;
+
+    /**
+     * @brief Constructor for the class.
+     *
+     * @param eventhandler points to the GameEventHandler.
+     * @param objectmanager points to the ObjectManager.
+     * @param owner points to the owning player.
+     * @param tilespaces (Optional) How many tilespaces does the building take.
+     * @param cost (Optional) How many resources does it cost to assign worker.
+     * @param efficiency (Optional) How good is the worker at his job.
+     */
     Teekkari(const std::shared_ptr<Course::iGameEventHandler> &eventhandler,
              const std::shared_ptr<Course::iObjectManager> &objectmanager,
              const std::shared_ptr<Course::PlayerBase> &owner,
@@ -17,13 +42,26 @@ public:
              const Course::ResourceMap &cost = GameResourceMaps::TEEKKARI_RECRUITMENT_COST,
              const Course::ResourceMapDouble &efficiency = GameResourceMaps::TEEKKARI_EFFICIENCY);
 
+    /**
+     * @brief Default destructor.
+     */
+    virtual ~Teekkari() = default;
+
+    /**
+     * @brief getType Returns a string describing objects type.
+     * @return std::string that represents Object's type.
+     * @post Exception guarantee: No-throw
+     */
     virtual std::string getType() const;
 
+    /**
+     * @brief Performs the Worker's default action.
+     */
     virtual void doSpecialAction() override;
 
 };
 
-}
+} // Namespace Game
 
 
 
